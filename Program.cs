7ea490a370338730d4e636ad0ecf873194124d3f
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2023-12-05 08:13:09 +0100

    First commit

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("---------------------------------------------------!");
+            Console.WriteLine("-- AI D A TO R E R A B | K O M M A N D O T O L K --!");
+            Console.WriteLine("---------------------------------------------------!");
         }
     }
 }
