1445a8386cc63385ae769e13fd459a47cb6df9b1
WebbkodsLarlingen <webbkodslarlingen@gmail.com>
2023-12-05 08:10:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace maka2207_projekt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
